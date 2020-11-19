a6a0e1f9298a62f829652235592cada2ffac98a7
salomaestro <chris10an.salomonsen@gmail.com>
2020-11-19 21:13:52 +0100

    Program which makes video into single frame images

---
+++ video_to_images.py
@@ -0,0 +1,50 @@
+# Importing all necessary libraries
+import cv2
+import os
+
+# path
+def main():
+    cwd = os.getcwd()
+    path = os.path.join(cwd, "video_ML.mp4")
+
+    # Read the video from specified path
+    cam = cv2.VideoCapture(path)
+
+    try:
+
+        # creating a folder named data
+        if not os.path.exists('dataimages'):
+            os.makedirs('dataimages')
+
+    # if not created then raise error
+    except OSError:
+        print ('Error: Creating directory of data')
+
+    # frame
+    currentframe = 0
+
+    while(True):
+
+        # reading from frame
+        ret,frame = cam.read()
+
+        if ret:
+            # if video is still left continue creating images
+            name = './dataimages/frame' + str(currentframe) + '.jpg'
+            print ('Creating...' + name)
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
